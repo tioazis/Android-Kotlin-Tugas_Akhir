16b733f81340ac4772b805bc191eb7f08ee46fa4
Azis Jabbar Susetio <tioazis@gmail.com>
2018-07-09 06:21:29 +0700

    Commit Pertama

---
+++ app/src/main/java/com/brid/azis/vipgame/test/Activity/MainMenu.kt
@@ -0,0 +1,16 @@
+package com.brid.azis.vipgame.test.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.brid.azis.vipgame.R
+import kotlinx.android.synthetic.main.activity_main_menu.*
+
+
+class MainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+
+    }
+}
---
+++ app/src/main/java/com/brid/azis/vipgame/test/Activity/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.brid.azis.vipgame.test.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.brid.azis.vipgame.R
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
